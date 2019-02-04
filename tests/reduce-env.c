ebf55d58acf1f4efa2bb167c7e11d8bfcfedfc43
Hugues Cassé <casse@irit.fr>
2019-02-04 16:43:13 +0100

    lexer: added // and /* */ support. cst: added reduce-env.c.

---
+++ tests/reduce-env.c
@@ -0,0 +1,25 @@
+
+void f(int x) {
+	int n = (4 * 10) << 2;	/* réductible */
+	int m = n / 2;			/* réductible */
+	
+	int k;
+	if(x > 0)
+		k = 0;
+	else
+		k = 0;
+	int kk;
+	kk = k + 1;			/* réductible */
+	int kkk = 0;
+	
+	while(m < n) {
+		kkk = 1;
+		k = 1;
+		m++;
+	}
+	
+	int ll = kk - 1;	/* réductible */
+	int lll;
+	lll = kkk + 1;	/* non-réductible */
+	
+}
